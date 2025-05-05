a10a708c4ea58341cd0beb71bd9197d7d46a8c7f
proyectos-jogal <jogal1965jjgh@gmail.com>
2025-05-05 16:55:57 -0300

    Actualizando data

--- data.py
+++ data.py
@@ -6,7 +6,7 @@ turistic_sites = [
         "ubicacion": "Bolívar",
         "clasificacion": "Selva",
         "atractivo": "La cascada más alta del mundo.",
-        "imagen": "https://raw.githubusercontent.com/tu_usuario/vzla_API/main/static/imagenes/salto_angel.jpg",
+        "imagen": "https://raw.githubusercontent.com/JoseLuisGalvis/api_venezuela_es_un_paraiso/main/static/images/salto_angel.webp",
         "caracteristicas": [
             "Altura de 979 metros.",
             "Ubicada en el Parque Nacional Canaima.",
@@ -19,6 +19,7 @@ turistic_sites = [
         "ubicacion": "Dependencias Federales",
         "clasificacion": "Playa",
         "atractivo": "Parque Nacional con playas vírgenes y arrecifes de coral.",
+        "imagen": "https://raw.githubusercontent.com/JoseLuisGalvis/api_venezuela_es_un_paraiso/main/static/images/los_roques.webp",
         "caracteristicas": [
             "Ideal para buceo y snorkel.",
             "Gran diversidad de fauna marina.",
@@ -31,6 +32,7 @@ turistic_sites = [
         "ubicacion": "Mérida",
         "clasificacion": "Montaña",
         "atractivo": "Sistema de teleférico más alto y largo del mundo.",
+        "imagen": "https://raw.githubusercontent.com/JoseLuisGalvis/api_venezuela_es_un_paraiso/main/static/images/teleferico_merida.webp",
         "caracteristicas": [
             "Ascenso a 4.765 metros sobre el nivel del mar.",
             "Vistas panorámicas de la Sierra Nevada.",
@@ -43,6 +45,7 @@ turistic_sites = [
         "ubicacion": "Monagas",
         "clasificacion": "Montaña",
         "atractivo": "Hogar del guácharo, ave nocturna única.",
+        "imagen": "https://raw.githubusercontent.com/JoseLuisGalvis/api_venezuela_es_un_paraiso/main/static/images/cueva_guacharo.webp",
         "caracteristicas": [
             "Recorridos guiados por la cueva.",
             "Observación de aves y murciélagos.",
@@ -55,6 +58,7 @@ turistic_sites = [
         "ubicacion": "Falcón",
         "clasificacion": "Playa",
         "atractivo": "Playas de arena blanca y manglares.",
+        "imagen": "https://raw.githubusercontent.com/JoseLuisGalvis/api_venezuela_es_un_paraiso/main/static/images/parque_morrocoy.webp",
         "caracteristicas": [
             "Ideal para deportes acuáticos.",
             "Diversidad de cayos y playas.",
@@ -67,6 +71,7 @@ turistic_sites = [
         "ubicacion": "Aragua y Carabobo",
         "clasificacion": "Montaña",
         "atractivo": "Primer parque nacional de Venezuela.",
+        "imagen": "https://raw.githubusercontent.com/JoseLuisGalvis/api_venezuela_es_un_paraiso/main/static/images/parque_pittier.webp",
         "caracteristicas": [
             "Gran biodiversidad.",
             "Senderos para caminatas.",
@@ -79,6 +84,7 @@ turistic_sites = [
         "ubicacion": "Falcón",
         "clasificacion": "Desierto",
         "atractivo": "Dunas de arena en un entorno desértico.",
+        "imagen": "https://raw.githubusercontent.com/JoseLuisGalvis/api_venezuela_es_un_paraiso/main/static/images/medanos_coro.webp",
         "caracteristicas": [
             "Actividades como sandboarding.",
             "Paisajes únicos en Venezuela.",
@@ -91,6 +97,7 @@ turistic_sites = [
         "ubicacion": "Anzoátegui y Sucre",
         "clasificacion": "Playa",
         "atractivo": "Playas y bahías de gran belleza.",
+        "imagen": "https://raw.githubusercontent.com/JoseLuisGalvis/api_venezuela_es_un_paraiso/main/static/images/parque_mochima.webp",
         "caracteristicas": [
             "Ideal para buceo y snorkel.",
             "Diversidad de fauna marina.",
@@ -103,6 +110,7 @@ turistic_sites = [
         "ubicacion": "Bolívar",
         "clasificacion": "Montaña",
         "atractivo": "Tepuy más famoso de Venezuela.",
+        "imagen": "https://raw.githubusercontent.com/JoseLuisGalvis/api_venezuela_es_un_paraiso/main/static/images/parque_roraima.webp",
         "caracteristicas": [
             "Excursiones de varios días.",
             "Paisajes únicos y formaciones rocosas.",
@@ -115,6 +123,7 @@ turistic_sites = [
         "ubicacion": "Delta Amacuro",
         "clasificacion": "Selva",
         "atractivo": "Uno de los deltas más grandes del mundo.",
+        "imagen": "https://raw.githubusercontent.com/JoseLuisGalvis/api_venezuela_es_un_paraiso/main/static/images/delta_orinoco.webp",
         "caracteristicas": [
             "Recorridos en curiara (embarcación típica).",
             "Observación de flora y fauna.",
@@ -127,6 +136,7 @@ turistic_sites = [
         "ubicacion": "Mérida y Barinas",
         "clasificacion": "Montaña",
         "atractivo": "Picos nevados y lagunas de alta montaña.",
+        "imagen": "https://raw.githubusercontent.com/JoseLuisGalvis/api_venezuela_es_un_paraiso/main/static/images/sierra_nevada.webp",
         "caracteristicas": [
             "Senderismo y escalada.",
             "Paisajes andinos.",
@@ -139,6 +149,7 @@ turistic_sites = [
         "ubicacion": "Nueva Esparta",
         "clasificacion": "Playa",
         "atractivo": "Destino turístico con playas y centros comerciales.",
+        "imagen": "https://raw.githubusercontent.com/JoseLuisGalvis/api_venezuela_es_un_paraiso/main/static/images/isla_margarita.webp",
         "caracteristicas": [
             "Variedad de playas.",
             "Actividades acuáticas.",
@@ -151,6 +162,7 @@ turistic_sites = [
         "ubicacion": "Miranda",
         "clasificacion": "Montaña",
         "atractivo": "Bosque nublado y ríos.",
+        "imagen": "https://raw.githubusercontent.com/JoseLuisGalvis/api_venezuela_es_un_paraiso/main/static/images/parque_roraima.webp",
         "caracteristicas": [
             "Senderos para caminatas.",
             "Observación de aves.",
@@ -163,6 +175,7 @@ turistic_sites = [
         "ubicacion": "Mérida",
         "clasificacion": "Montaña",
         "atractivo": "Paisajes andinos y lagunas.",
+        "imagen": "https://raw.githubusercontent.com/JoseLuisGalvis/api_venezuela_es_un_paraiso/main/static/images/sierra_culata.webp",
         "caracteristicas": [
             "Senderismo y escalada.",
             "Observación de fauna.",
@@ -187,6 +200,7 @@ turistic_sites = [
         "ubicacion": "Bolívar",
         "clasificacion": "Selva",
         "atractivo": "Bosques tropicales y ríos.",
+        "imagen": "https://raw.githubusercontent.com/JoseLuisGalvis/api_venezuela_es_un_paraiso/main/static/images/parque_yurubi.webp",
         "caracteristicas": [
             "Senderos ecológicos.",
             "Observación de fauna.",
@@ -199,6 +213,7 @@ turistic_sites = [
         "ubicacion": "Monagas",
         "clasificacion": "Montaña",
         "atractivo": "Hogar del guácharo y otras aves.",
+        "imagen": "https://raw.githubusercontent.com/JoseLuisGalvis/api_venezuela_es_un_paraiso/main/static/images/parque_guacharo.webp",
         "caracteristicas": [
             "Recorridos guiados por la cueva.",
             "Observación de aves y murciélagos.",
@@ -211,6 +226,7 @@ turistic_sites = [
         "ubicacion": "Nueva Esparta",
         "clasificacion": "Playa",
         "atractivo": "Laguna y manglares.",
+        "imagen": "https://raw.githubusercontent.com/JoseLuisGalvis/api_venezuela_es_un_paraiso/main/static/images/laguna_larestinga.webp",
         "caracteristicas": [
             "Recorridos en bote.",
             "Observación de aves.",
@@ -223,6 +239,7 @@ turistic_sites = [
         "ubicacion": "Distrito Capital y Miranda",
         "clasificacion": "Montaña",
         "atractivo": "Montaña Ávila y teleférico.",
+        "imagen": "https://raw.githubusercontent.com/JoseLuisGalvis/api_venezuela_es_un_paraiso/main/static/images/cerro_avila.webp",
         "caracteristicas": [
             "Senderos para caminatas.",
             "Vistas panorámicas de Caracas.",
@@ -235,6 +252,7 @@ turistic_sites = [
         "ubicacion": "Lara",
         "clasificacion": "Montaña",
         "atractivo": "Bosques nublados y ríos.",
+        "imagen": "https://raw.githubusercontent.com/JoseLuisGalvis/api_venezuela_es_un_paraiso/main/static/images/parque_dinira.webp",
         "caracteristicas": [
             "Senderos para caminatas.",
             "Observación de aves.",
@@ -247,6 +265,7 @@ turistic_sites = [
         "ubicacion": "Lara",
         "clasificacion": "Montaña",
         "atractivo": "Bosques secos y ríos.",
+        "imagen": "https://raw.githubusercontent.com/JoseLuisGalvis/api_venezuela_es_un_paraiso/main/static/images/parque_terepaima.webp",
         "caracteristicas": [
             "Senderos para caminatas.",
             "Observación de fauna.",
@@ -259,6 +278,7 @@ turistic_sites = [
         "ubicacion": "Anzoátegui",
         "clasificacion": "Montaña",
         "atractivo": "Bosques secos y ríos.",
+        "imagen": "https://raw.githubusercontent.com/JoseLuisGalvis/api_venezuela_es_un_paraiso/main/static/images/cerro_saroche.webp",
         "caracteristicas": [
             "Senderos para caminatas.",
             "Observación de aves.",
@@ -271,6 +291,7 @@ turistic_sites = [
         "ubicacion": "Distrito Capital y Miranda",
         "clasificacion": "Montaña",
         "atractivo": "Bosques nublados y ríos.",
+        "imagen": "https://raw.githubusercontent.com/JoseLuisGalvis/api_venezuela_es_un_paraiso/main/static/images/parque_macarao.webp",
         "caracteristicas": [
             "Senderos para caminatas.",
             "Observación de aves.",
@@ -283,6 +304,7 @@ turistic_sites = [
         "ubicacion": "Delta Amacuro",
         "clasificacion": "Selva",
         "atractivo": "Bosques tropicales y ríos.",
+        "imagen": "https://raw.githubusercontent.com/JoseLuisGalvis/api_venezuela_es_un_paraiso/main/static/images/parque_turuepano.webp",
         "caracteristicas": [
             "Senderos ecológicos.",
             "Observación de fauna.",
